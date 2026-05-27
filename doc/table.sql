432923710a7d96e0e93928050b89d44cae8a7b0f
lwxcd <l1393452711@gmail.com>
2026-05-27 16:08:28 +0800

    修正sql文件错误

--- doc/table.sql
+++ doc/table.sql
@@ -6,8 +6,8 @@ create table contest
     status          varchar(16) not null comment '状态',
     create_user_id  bigint      not null,
     create_group_id bigint      null,
-    create_time     timestamp   not null,
-    update_time     timestamp   not null
+    create_time     timestamp   not null default CURRENT_TIMESTAMP,
+    update_time     timestamp   not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 )
     comment '比赛';
 
@@ -22,8 +22,8 @@ create table contest_end
     user_id     bigint         null,
     end_point   decimal(10, 2) not null comment '结算点数',
     elo_change  decimal(10, 2) null comment 'elo变化',
-    create_time timestamp      not null,
-    update_time timestamp      not null,
+    create_time timestamp      not null default CURRENT_TIMESTAMP,
+    update_time timestamp      not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
     constraint contest_end_contest_id_user_id_uindex
         unique (contest_id, user_id)
 )
@@ -37,8 +37,8 @@ create table contest_record
     direction      varchar(16) not null comment 'direction',
     record_user_id bigint      not null comment '用户id',
     point          int         not null comment '点数',
-    create_time    timestamp   not null,
-    update_time    timestamp   not null,
+    create_time    timestamp   not null default CURRENT_TIMESTAMP,
+    update_time    timestamp   not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
     constraint contest_record_contest_id_direction_uindex
         unique (contest_id, direction)
 )
@@ -51,8 +51,8 @@ create table elo
     user_id     bigint                         not null,
     type        varchar(16)                    null,
     elo         decimal(10, 2) default 2000.00 not null,
-    update_time timestamp                      not null,
-    create_time timestamp                      not null,
+    create_time timestamp                      not null default CURRENT_TIMESTAMP,
+    update_time timestamp                      not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
     constraint elo_type_user_id_uindex
         unique (type, user_id)
 )
